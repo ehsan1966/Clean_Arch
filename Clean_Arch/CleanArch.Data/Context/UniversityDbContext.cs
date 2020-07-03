dbe3646bb034bcbeb00d759367462eba437a463a
Ehsan-Pc <behbahanie@yahoo.com>
2020-07-03 15:47:55 +0430

    Add Context

---
+++ Clean_Arch/CleanArch.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Domains.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Data.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}
